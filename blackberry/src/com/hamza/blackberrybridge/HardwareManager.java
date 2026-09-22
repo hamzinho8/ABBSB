3bcd79edc265bb54370ba9139a0b5eb094e3f823
htic <houamhamza8@gmail.com>
2026-09-22 12:06:04 +0000

    feat: bump to v1.0.7 and remove restricted APIs
    Refactor HardwareManager to use standard J2ME MMAPI instead of RIM-specific APIs to avoid security permission errors on BlackBerry devices. Added required Bluetooth permissions to the manifest.

--- blackberry/src/com/hamza/blackberrybridge/HardwareManager.java
+++ blackberry/src/com/hamza/blackberrybridge/HardwareManager.java
@@ -1,116 +1,49 @@
 package com.hamza.blackberrybridge;
 
-import net.rim.device.api.notification.NotificationsManager;
-import net.rim.device.api.notification.NotificationsConstants;
-import net.rim.device.api.system.Alert;
-import net.rim.device.api.system.LED;
 import java.util.Timer;
 import java.util.TimerTask;
+import javax.microedition.media.Manager;
 
 public class HardwareManager {
-    private static Timer vibrateTimer;
     private static Timer findPhoneTimer;
-    private static final long NOTIF_ID = 0x5a3b92c4L; // Unique ID for SmartBridge
-    private static final short[] BEEP_TUNE = new short[] { 1760, 200, 0, 50, 1760, 200 };
-
-    static {
-        try {
-            // Register application notification source in BlackBerry OS
-            NotificationsManager.registerSource(NOTIF_ID, "SmartBridge", NotificationsConstants.DEFAULT_LEVEL);
-        } catch (Throwable t) {}
-    }
 
     public static void triggerMessageAlert() {
         try {
-            if (Alert.isVibrateSupported()) {
-                Alert.startVibrate(500);
-            }
-        } catch (Throwable t) {}
-        try {
-            LED.setState(LED.STATE_BLINKING);
-        } catch (Throwable t) {}
-        try {
-            NotificationsManager.triggerImmediateEvent(NOTIF_ID, 0, null, null);
+            // Standard J2ME MMAPI tone (A4 note, 150ms, volume 80)
+            // 100% open and unrestricted - does not require RIM code signing keys
+            Manager.playTone(69, 150, 80);
         } catch (Throwable t) {}
     }
     
     public static void stopMessageAlert() {
-        try {
-            LED.setState(LED.STATE_OFF);
-        } catch (Throwable t) {}
-        try {
-            NotificationsManager.cancelImmediateEvent(NOTIF_ID, 0, null, null);
-        } catch (Throwable t) {}
+        // No-op
     }
     
     public static void triggerNotificationAlert(SmartBridgeApp app, String appName) {
-        try {
-            if (Alert.isVibrateSupported()) {
-                Alert.startVibrate(500);
-            }
-        } catch (Throwable t) {}
-        try {
-            LED.setState(LED.STATE_BLINKING);
-        } catch (Throwable t) {}
-        try {
-            NotificationsManager.triggerImmediateEvent(NOTIF_ID, 0, null, null);
-        } catch (Throwable t) {}
+        // Audio playback is handled cleanly by app.getAudioManager().playNotificationSound(appName)
     }
     
     public static void triggerCallAlert(SmartBridgeApp app) {
         stopAlerts();
-        try {
-            LED.setState(LED.STATE_BLINKING);
-        } catch (Throwable t) {}
-        try {
-            NotificationsManager.triggerImmediateEvent(NOTIF_ID, 0, null, null);
-        } catch (Throwable t) {}
-        vibrateTimer = new Timer();
-        vibrateTimer.scheduleAtFixedRate(new TimerTask() {
-            public void run() {
-                try {
-                    if (Alert.isVibrateSupported()) {
-                        Alert.startVibrate(600);
-                    }
-                } catch (Throwable t) {}
-            }
-        }, 0, 1000);
+        if (app != null && app.getAudioManager() != null) {
+            app.getAudioManager().playCallRingtone();
+        }
     }
     
     public static void stopAlerts() {
-        if (vibrateTimer != null) {
-            vibrateTimer.cancel();
-            vibrateTimer = null;
-        }
-        try {
-            LED.setState(LED.STATE_OFF);
-        } catch (Throwable t) {}
-        try {
-            NotificationsManager.cancelImmediateEvent(NOTIF_ID, 0, null, null);
-        } catch (Throwable t) {}
+        stopFindPhoneAlert();
     }
     
     public static void startFindPhoneAlert() {
         stopFindPhoneAlert();
-        try {
-            LED.setState(LED.STATE_BLINKING);
-        } catch (Throwable t) {}
-        try {
-            NotificationsManager.triggerImmediateEvent(NOTIF_ID, 0, null, null);
-        } catch (Throwable t) {}
         findPhoneTimer = new Timer();
         findPhoneTimer.scheduleAtFixedRate(new TimerTask() {
             public void run() {
                 try {
-                    Alert.startAudio(BEEP_TUNE, 100);
-                } catch (Throwable t) {}
-                try {
-                    if (Alert.isVibrateSupported()) {
-                        Alert.startVibrate(500);
-                    }
+                    Manager.playTone(81, 350, 100);
                 } catch (Throwable t) {}
             }
-        }, 0, 1000);
+        }, 0, 700);
     }
     
     public static void stopFindPhoneAlert() {
@@ -118,11 +51,6 @@ public class HardwareManager {
             findPhoneTimer.cancel();
             findPhoneTimer = null;
         }
-        try {
-            LED.setState(LED.STATE_OFF);
-        } catch (Throwable t) {}
-        try {
-            NotificationsManager.cancelImmediateEvent(NOTIF_ID, 0, null, null);
-        } catch (Throwable t) {}
     }
 }
+
